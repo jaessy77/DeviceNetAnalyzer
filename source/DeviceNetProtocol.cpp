b807ea48f9d0f5e4a3a3254587c4d07721a6de0a
Marco Jassmann <jaessy77@gmail.com>
2018-11-29 21:27:29 +0100

    ArbitrationField and ControlField implemented

--- source/DeviceNetProtocol.cpp
+++ source/DeviceNetProtocol.cpp
@@ -15,34 +15,194 @@ DeviceNetProtocol::DeviceNetProtocol()
 	mMacID = 0;				// Member in Message Group 2 ; either Source or Destination MAC ID
 	mGroup2MessageID = 0;	// Member in Message Group 2
 	mGroup3MessageID = 0;	// Member in Message Group 3
-	mSourceMacID_MG2 = 0;	// Member in Message Group 3
+	mSourceMacID_MG3 = 0;	// Member in Message Group 3
 	mGroup4MessageID = 0;	// Member in Message Group 4
 
 	// Other useful variables
 	mIdentifierError = false;
+	mRtrBitError = false;
+	mR1R0BitsError = false;
+	mDataLegthError = false;
+
+	// Group 2 Message ID specials
+	mReservedMacIdConnectionManagement = false;
+	mDuplicateMacIdCheckMessage = false;
+
+	// Group 3 Message ID specials
+	mExplicitResponseMessage = false;
+	mExplicitRequestMessage = false;
+	mReservedGroup3MsgID = false;
+
+	// Group 4 Message ID specials
+	mReservedGroup4MsgID = false;
+	mCommunicationFaultedResponseMessage = false;
+	mCommunicationFaultedRequestMessage = false;
+	mOfflineOwnershipResponseMessage = false;
+	mOfflineOwnershipRequestMessage = false;
+
+	// Field variables
+	mArbitrationFieldIdentifierBits = 0;
+	mArbitrationFieldRtrBit = BIT_RTR;
+	mArbitrationField = 0;
+
+	mControlFieldReservedBits = BIT_R1 | BIT_R0;
+	mControlFieldDataLengthCodeBits = 0;
+	mControlField = 0;
+
+
 }
 
 DeviceNetProtocol::~DeviceNetProtocol()
 {
 }
 
-
-U32 DeviceNetProtocol::ComposeGroup1MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1)
+void DeviceNetProtocol::ComposeGroup1MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1)
 {
-	return U32();
+	mArbitrationFieldIdentifierBits = BITS_MESSAGE_GROUP_1 | (mGroup1MessageID << SHIFT_GROUP_1_MESSAGE_ID) | (mSourceMacID_MG1 << SHIFT_SOURCE_MAC_ID);
 }
 
-U32 DeviceNetProtocol::ComposeGroup2MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1)
+void DeviceNetProtocol::ComposeGroup2MessageIdentifier(U32 mGroup2MessageID, U32 mMacID)
 {
-	return U32();
+	mArbitrationFieldIdentifierBits = BITS_MESSAGE_GROUP_2 | (mGroup2MessageID << SHIFT_GROUP_2_MESSAGE_ID) | (mMacID << SHIFT_MAC_ID);
 }
 
-U32 DeviceNetProtocol::ComposeGroup3MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1)
+void DeviceNetProtocol::ComposeGroup3MessageIdentifier(U32 mGroup3MessageID, U32 mSourceMacID_MG3)
 {
-	return U32();
+	mArbitrationFieldIdentifierBits = BITS_MESSAGE_GROUP_3 | (mGroup3MessageID << SHIFT_GROUP_3_MESSAGE_ID) | (mSourceMacID_MG3 << SHIFT_SOURCE_MAC_ID);
 }
 
-U32 DeviceNetProtocol::ComposeGroup4MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1)
+void DeviceNetProtocol::ComposeGroup4MessageIdentifier(U32 mGroup4MessageID)
 {
-	return U32();
+	if (mGroup4MessageID > MAX_VAL_GROUP_4_MESSAGE_ID)
+	{
+		mArbitrationFieldIdentifierBits = 0xFFFFFFFF;
+	}
+		
+	else
+	{
+		mArbitrationFieldIdentifierBits = BITS_MESSAGE_GROUP_4 | (mGroup4MessageID << SHIFT_GROUP_4_MESSAGE_ID);
+	}	
+}
+
+void DeviceNetProtocol::ComposeArbitrationField(void)
+{
+	mArbitrationField = (mArbitrationFieldIdentifierBits << 1) | (mArbitrationFieldRtrBit << 0);
+
+}
+
+void DeviceNetProtocol::ComposeControlField(U32 mDataLengthCode)
+{
+	mControlFieldDataLengthCodeBits = mDataLengthCode;
+	mControlField = (mControlFieldReservedBits << 4) | (mControlFieldDataLengthCodeBits << 0);
+}
+
+void DeviceNetProtocol::DecomposeArbitrationField(U32 mArbitrationField)
+{
+	mArbitrationFieldIdentifierBits = ((mArbitrationField & 0x00000FFE ) >> 1);
+	mArbitrationFieldRtrBit = ((mArbitrationField & 0x00000001) >> 0);
+
+	// Decode the identifier-bits to figure out the Message Group
+	if ((mArbitrationFieldIdentifierBits >= START_ADDR_MESSAGE_GROUP_1) && (mArbitrationFieldIdentifierBits < START_ADDR_MESSAGE_GROUP_2))
+	{
+		mMessageGroup1 = true;
+		mGroup1MessageID = ((mArbitrationFieldIdentifierBits & MASK_GROUP_1_MESSAGE_ID) >> SHIFT_GROUP_1_MESSAGE_ID);
+		mSourceMacID_MG1 = ((mArbitrationFieldIdentifierBits & MASK_SOURCE_MAC_ID) >> SHIFT_SOURCE_MAC_ID);
+	}
+	else if ((mArbitrationFieldIdentifierBits >= START_ADDR_MESSAGE_GROUP_2) && (mArbitrationFieldIdentifierBits < START_ADDR_MESSAGE_GROUP_3))
+	{
+		mMessageGroup2 = true;
+		mMacID = ((mArbitrationFieldIdentifierBits & MASK_MAC_ID) >> SHIFT_MAC_ID);
+		mGroup2MessageID = ((mArbitrationFieldIdentifierBits & MASK_GROUP_2_MESSAGE_ID) >> SHIFT_GROUP_2_MESSAGE_ID);
+		if (mGroup2MessageID == CHECK_GROUP_2_MSG_ID_IS_CONNECTION_MANAGEMENT)
+		{
+			mReservedMacIdConnectionManagement = true;
+		}
+		if (mGroup2MessageID == CHECK_GROUP_2_MSG_ID_IS_CHECK_MESSAGE)
+		{
+			mDuplicateMacIdCheckMessage = true;
+		}
+	}
+	else if ((mArbitrationFieldIdentifierBits >= START_ADDR_MESSAGE_GROUP_3) && (mArbitrationFieldIdentifierBits < START_ADDR_MESSAGE_GROUP_4))
+	{
+		mMessageGroup3 = true;
+
+		mGroup3MessageID = ((mArbitrationFieldIdentifierBits & MASK_GROUP_3_MESSAGE_ID) >> SHIFT_GROUP_3_MESSAGE_ID);
+
+		if (mGroup3MessageID == CHECK_GROUP_3_MSG_ID_IS_EXPLICIT_RESPONSE)
+		{
+			mExplicitResponseMessage = true;
+		}
+		if (mGroup3MessageID == CHECK_GROUP_3_MSG_ID_IS_EXPLICIT_REQUEST)
+		{
+			mExplicitRequestMessage = true;
+		}
+		if (mGroup3MessageID == CHECK_GROUP_3_MSG_ID_IS_RESERVED)
+		{
+			mReservedGroup3MsgID = true;
+		}
+
+		mSourceMacID_MG3 = ((mArbitrationFieldIdentifierBits & MASK_SOURCE_MAC_ID) >> SHIFT_SOURCE_MAC_ID);
+	}
+	else if ((mArbitrationFieldIdentifierBits >= START_ADDR_MESSAGE_GROUP_4) && (mArbitrationFieldIdentifierBits < START_ADDR_INVALID_CAN_IDS))
+	{
+		mMessageGroup4 = true;
+
+		mGroup4MessageID = ((mArbitrationFieldIdentifierBits & MASK_GROUP_4_MESSAGE_ID) >> SHIFT_GROUP_4_MESSAGE_ID);
+
+		if (mGroup4MessageID == CHECK_GROUP_4_MSG_ID_IS_COM_FAULTED_RESPONSE)
+		{
+			mCommunicationFaultedResponseMessage = true;
+		}
+		else if (mGroup4MessageID == CHECK_GROUP_4_MSG_ID_IS_COM_FAULTED_REQUEST)
+		{
+			mCommunicationFaultedRequestMessage = true;
+		}
+		else if (mGroup4MessageID == CHECK_GROUP_4_MSG_ID_IS_OFFLINE_OWNERSHIP_RESPONSE)
+		{
+			mOfflineOwnershipResponseMessage = true;
+		}
+		else if (mGroup4MessageID == CHECK_GROUP_4_MSG_ID_IS_OFFLINE_OWNERSHIP_REQUEST)
+		{
+			mOfflineOwnershipRequestMessage = true;
+		}
+		else
+		{
+			mReservedGroup4MsgID = true;
+		}
+	}
+	else if ((mArbitrationFieldIdentifierBits >= START_ADDR_INVALID_CAN_IDS) && (mArbitrationFieldIdentifierBits < START_ADDR_INVALID_IDENTIFIER))
+	{
+		mInvalidCanIdentifiers = true;
+	}
+	else
+	{
+		mIdentifierError = true;
+	}
+
+	if (mArbitrationFieldRtrBit != BIT_RTR)
+	{
+		mRtrBitError = true;
+	}
+}
+
+void DeviceNetProtocol::DecomposeControlField(U32 mControlField)
+{
+	mControlFieldReservedBits = ((mControlField & 0x00000030) >> 4);
+	mControlFieldDataLengthCodeBits = ((mControlField & 0x0000000F) >> 0);
+
+	if (mControlFieldReservedBits != (BIT_R1 | BIT_R0))
+	{
+		mR1R0BitsError = true;
+	}
+
+	if (mControlFieldDataLengthCodeBits == 0)
+	{
+		mDataLegthError = true;
+		mDataLength = 0;
+	}
+	else
+	{
+		mDataLength = mControlFieldDataLengthCodeBits;
+	}
+
 }
--- source/DeviceNetProtocol.h
+++ source/DeviceNetProtocol.h
@@ -58,42 +58,107 @@
 	|___________|............===>	Invalid CAN ID		Bit 10:4 always '1111111'
 */
 
-// Address-Ranges
-#define MESSAGE_GROUP_1_START		0x00000000
-#define MESSAGE_GROUP_1_END			0x000003FF
-#define MESSAGE_GROUP_2_START		0x00000400
-#define MESSAGE_GROUP_2_END			0x000005FF
-#define MESSAGE_GROUP_3_START		0x00000600
-#define MESSAGE_GROUP_3_END			0x000007BF
-#define MESSAGE_GROUP_4_START		0x000007C0
-#define MESSAGE_GROUP_4_END			0x000007EF
-#define INVALID_CAN_IDS_START		0x000007F0
-#define INVALID_CAN_IDS_END			0x000007FF
-#define INVALID_IDENTIFIER_START	0x00000800
+// Address-Ranges MESSAGE GROUP's
+#define START_ADDR_MESSAGE_GROUP_1		0x00000000
+#define END_ADDR_MESSAGE_GROUP_1		0x000003FF
+#define START_ADDR_MESSAGE_GROUP_2		0x00000400
+#define END_ADDR_MESSAGE_GROUP_2		0x000005FF
+#define START_ADDR_MESSAGE_GROUP_3		0x00000600
+#define END_ADDR_MESSAGE_GROUP_3		0x000007BF
+#define START_ADDR_MESSAGE_GROUP_4		0x000007C0
+#define END_ADDR_MESSAGE_GROUP_4		0x000007EF
+#define START_ADDR_INVALID_CAN_IDS		0x000007F0
+#define END_ADDR_INVALID_CAN_IDS		0x000007FF
+#define START_ADDR_INVALID_IDENTIFIER	0x00000800
+
+// Address-Ranges CLASS ID's
+#define START_ADDR_CLASS_ID_OPEN_1		0x00000000	// "Open"
+#define END_ADDR_CLASS_ID_OPEN_1		0x00000063
+#define START_ADDR_CLASS_ID_VENDOR_1	0x00000064	// "Vendor Specific"
+#define END_ADDR_CLASS_ID_VENDOR_1		0x000000C7
+#define START_ADDR_CLASS_ID_RESERVED_1	0x000000C8	// "Reserved by DeviceNet for future use"
+#define END_ADDR_CLASS_ID_RESERVED_1	0x000000FF
+#define START_ADDR_CLASS_ID_OPEN_2		0x00000100	// "Open"
+#define END_ADDR_CLASS_ID_OPEN_2		0x000002FF
+#define START_ADDR_CLASS_ID_VENDOR_2	0x00000300	// "Vendor Specific"
+#define END_ADDR_CLASS_ID_VENDOR_2		0x000004FF
+#define START_ADDR_CLASS_ID_RESERVED_2	0x00000500	// "Reserved by DeviceNet for future use"
+#define END_ADDR_CLASS_ID_RESERVED_2	0x0000FFFF
+
+// Address-Ranges SERVICE CODE's
+#define START_ADDR_SERVICE_CODE_OPEN		0x00000000	// "Open.  These are referred to as DeviceNet Common Services.  These are defined in Appendix G."
+#define END_ADDR_SERVICE_CODE_OPEN			0x00000031
+#define START_ADDR_SERVICE_CODE_VENDOR		0x00000032	// "Vendor Specific"
+#define END_ADDR_SERVICE_CODE_VENDOR		0x0000004A
+#define START_ADDR_SERVICE_CODE_OBJECT		0x0000004B	// "Object Class Specific"
+#define END_ADDR_SERVICE_CODE_OBJECT		0x00000063
+#define START_ADDR_SERVICE_CODE_RESERVED	0x00000064	// "Reserved by DeviceNet for future use"
+#define END_ADDR_SERVICE_CODE_RESERVED		0x0000007F
+#define START_ADDR_SERVICE_CODE_INVALID		0x00000080	// "Invalid/Not used"
+#define END_ADDR_SERVICE_CODE_INVALID		0x000000FF
+
+// Address-Ranges ATTRIBUTE ID's
+#define START_ADDR_ATTRIBUTE_ID_OPEN		0x00000000	// "Open"
+#define END_ADDR_ATTRIBUTE_ID_OPEN			0x00000063
+#define START_ADDR_ATTRIBUTE_ID_VENDOR		0x00000064	// "Vendor Specific"
+#define END_ADDR_ATTRIBUTE_ID_VENDOR		0x000000C7
+#define START_ADDR_ATTRIBUTE_ID_RESERVED	0x000000C8	// "Reserved by DeviceNet for future use"
+#define END_ADDR_ATTRIBUTE_ID_RESERVED		0x000000FF
+
+// Address-Ranges MAC ID's
+#define START_ADDR_MAC_ID	0x00000000	// "The MAC ID.  The value 63 (decimal) is to be utilized upon initialization of a device (e.g. powerup) if another value has not been assigned."
+#define END_ADDR_MAC_ID		0x0000003F
 
 // Masks to decode and shift the Member's in Message Group's
-#define GROUP_1_MESSAGE_ID_MASK		0x000003C0	// Bit 9:6
-#define GROUP_1_MESSAGE_ID_SHIFT	6
+#define MASK_GROUP_1_MESSAGE_ID		0x000003C0	// Bit 9:6
+#define SHIFT_GROUP_1_MESSAGE_ID	6
 
-#define SOURCE_MAC_ID_MASK			0x0000003F	// Bit 5:0 (Same position in MG1 and MG3)
-#define SOURCE_MAC_ID_SHIFT			0
+#define MASK_SOURCE_MAC_ID			0x0000003F	// Bit 5:0 (Same position in MG1 and MG3)
+#define SHIFT_SOURCE_MAC_ID			0
 
-#define MAC_ID_MASK					0x000001F8	// Bit 8:3
-#define MAC_ID_SHIFT				3
+#define MASK_MAC_ID					0x000001F8	// Bit 8:3
+#define SHIFT_MAC_ID				3
 
-#define GROUP_2_MESSAGE_ID_MASK		0x00000007	// Bit 2:0
-#define GROUP_2_MESSAGE_ID_SHIFT	0
+#define MASK_GROUP_2_MESSAGE_ID		0x00000007	// Bit 2:0
+#define SHIFT_GROUP_2_MESSAGE_ID	0
 
-#define GROUP_3_MESSAGE_ID_MASK		0x000001C0	// Bit 8:6
-#define GROUP_3_MESSAGE_ID_SHIFT	6
+#define MASK_GROUP_3_MESSAGE_ID		0x000001C0	// Bit 8:6
+#define SHIFT_GROUP_3_MESSAGE_ID	6
 
-#define GROUP_4_MESSAGE_ID_MASK		0x0000003F	// Bit 5:0
-#define GROUP_4_MESSAGE_ID_SHIFT	0
+#define MASK_GROUP_4_MESSAGE_ID		0x0000003F	// Bit 5:0
+#define SHIFT_GROUP_4_MESSAGE_ID	0
+
+// Identity usage bits in Field Identifier bit
+#define BITS_MESSAGE_GROUP_1			0x00000000 // Bit 10 is always '0'
+#define BITS_MESSAGE_GROUP_2			0x00000400 // Bit 10:9 is always '10'
+#define BITS_MESSAGE_GROUP_3			0x00000600 // Bit 10:9 is always '11'
+#define BITS_MESSAGE_GROUP_4			0x000007C0 // Bit 10:6 is always '11111'
+#define BITS_INVALID_CAN_IDENTIFIERS	0x000007F0 // Bit 10:4 is always '1111111' Bit 3:0 is 'XXXX' (Do not care)
+
+// Restriction Values MIN_VAL or MAX_VAL
+#define MAX_VAL_GROUP_4_MESSAGE_ID		0x0000002F
+#define MIN_VAL_INTERFRAME_SPACE_BITS	0x00000003	// Minimum 3 Bits "Recessive = '1' before next CAN-Frame"
+
+// Check for Special Values
+#define CHECK_GROUP_2_MSG_ID_IS_CONNECTION_MANAGEMENT		0x00000006	// "Reserved for Predefined Master/Slave Connection Management"
+#define CHECK_GROUP_2_MSG_ID_IS_CHECK_MESSAGE				0x00000007	// "Duplicate MAC ID Check Message"
+#define CHECK_GROUP_3_MSG_ID_IS_EXPLICIT_RESPONSE			0x00000005	// "Unconnected Explicit Response Messages"
+#define CHECK_GROUP_3_MSG_ID_IS_EXPLICIT_REQUEST			0x00000006	// "Unconnected Explicit Request Messages"
+#define CHECK_GROUP_3_MSG_ID_IS_RESERVED					0x00000007	// "Group 3 Message ID value 7 is invalid and is not used."
+#define CHECK_GROUP_4_MSG_ID_IS_COM_FAULTED_RESPONSE		0x0000002C	// "Communication Faulted Response Message"
+#define CHECK_GROUP_4_MSG_ID_IS_COM_FAULTED_REQUEST			0x0000002D	// "Communication Faulted Request Message"
+#define CHECK_GROUP_4_MSG_ID_IS_OFFLINE_OWNERSHIP_RESPONSE	0x0000002E	// "Offline Ownership Response Message"
+#define CHECK_GROUP_4_MSG_ID_IS_OFFLINE_OWNERSHIP_REQUEST	0x0000002F	// "Offline Ownership Request Message"
+
+// Useful defines
+#define BIT_RTR	0x00000000	// always dominant '0' since  DeviceNet does not make use of the CAN Remote Frame
+#define BIT_R1	0x00000000	// always dominant '0' since DeviceNet is based on CAN 2.0A where r1 is reserved.
+#define BIT_R0	0x00000000	// always dominant '0' since DeviceNet is based on CAN 2.0A where r0 is reserved.
 
 enum DeviceNetFrameType
 {
-	IdentifierField,
-	ControlField,
+	ArbitrationField,	// 11 bit Identifier, 1 bit RTR (always dominant '0' since  DeviceNet does not make use of the CAN Remote Frame)
+	ControlField,		// 2 bit Reserved (r1,r0 always '00'), 4 bit Data lenght code
 	DataField,
 	CrcField,
 	AckField,
@@ -116,22 +181,59 @@ public:
 	// ID-Types included in identifier bits
 	U32 mGroup1MessageID;	// Member in Message Group 1	
 	U32 mSourceMacID_MG1;	// Member in Message Group 1
-	U32 mMacID;				// Member in Message Group 2 ; either Source or Destination MAC ID
+	U32 mMacID;				// Member in Message Group 2 ; "either Source or Destination MAC ID"
 	U32 mGroup2MessageID;	// Member in Message Group 2
 	U32 mGroup3MessageID;	// Member in Message Group 3
-	U32 mSourceMacID_MG2;	// Member in Message Group 3
+	U32 mSourceMacID_MG3;	// Member in Message Group 3
 	U32 mGroup4MessageID;	// Member in Message Group 4
 
 	// Other useful variables
 	bool mIdentifierError;
+	bool mRtrBitError;
+	bool mR1R0BitsError;
+	bool mDataLegthError;
+
+	// Group 2 Message ID specials
+	bool mReservedMacIdConnectionManagement;	// Group 2 Message ID is '110' ; "Reserved for Predefined Master/Slave Connection Management"
+	bool mDuplicateMacIdCheckMessage;			// Group 2 Message ID is '111' ; "Duplicate MAC ID Check Message"
+
+	// Group 3 Message ID specials
+	// Important: Use of Group 3 Message ID values 5, 6 and 7 is reserved by DeviceNet.
+	bool mExplicitResponseMessage;				// Group 3 Message ID is '101' ; "Unconnected Explicit Response Messages"
+	bool mExplicitRequestMessage;				// Group 3 Message ID is '110' ; "Unconnected Explicit Request Messages"
+	bool mReservedGroup3MsgID;					// Group 3 Message ID is '111' ; "Group 3 Message ID value 7 is invalid and is not used."
+
+	// Group 4 Message ID specials
+	bool mReservedGroup4MsgID;					// Group 4 Message ID from '000000' to '101011' ; "Reserved Group 4 Message"
+	bool mCommunicationFaultedResponseMessage;	// Group 4 Message ID  is '101100' ; "Communication Faulted Response Message"
+	bool mCommunicationFaultedRequestMessage;	// Group 4 Message ID  is '101101' ; "Communication Faulted Request Message"
+	bool mOfflineOwnershipResponseMessage;		// Group 4 Message ID  is '101110' ; "Offline Ownership Response Message"
+	bool mOfflineOwnershipRequestMessage;		// Group 4 Message ID  is '101111' ; "Offline Ownership Request Message"
+	
+	U32 mDataLength;
 
 	// Field variables
-	U32 mIdentifierBits;	// 11 bits long
+	U32 mArbitrationFieldIdentifierBits;	// 11 bits long
+	U32 mArbitrationFieldRtrBit;			// 1 bit long, always '0'
+	U32 mArbitrationField;					// 12 bits ( mArbitrationFieldIdentifierBits + mArbitrationFieldRtrBit)
+	
+	U32 mControlFieldReservedBits;			// 2 bit long, always '00'
+	U32 mControlFieldDataLengthCodeBits;	// 4 bit long
+	U32 mControlField;						// 6 bits long (mControlFieldReservedBits + mControlFieldDataLengthCodeBits)
+	
+	// Simulation dependend functions
+	void ComposeGroup1MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1);
+	void ComposeGroup2MessageIdentifier(U32 mGroup2MessageID, U32 mMacID);
+	void ComposeGroup3MessageIdentifier(U32 mGroup3MessageID, U32 mSourceMacID_MG3);
+	void ComposeGroup4MessageIdentifier(U32 mGroup4MessageID);
+	void ComposeArbitrationField();
 
-	U32 ComposeGroup1MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1);
-	U32 ComposeGroup2MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1);
-	U32 ComposeGroup3MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1);
-	U32 ComposeGroup4MessageIdentifier(U32 mGroup1MessageID, U32 mSourceMacID_MG1);
+	void ComposeControlField(U32 mDataLengthCode);
+
+	// Analyzing dependend functions
+	void DecomposeArbitrationField(U32 mArbitrationField);
+
+	void DecomposeControlField(U32 mControlField);
 
 protected:
 	
